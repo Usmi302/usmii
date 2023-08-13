b62bd2b77aae83e1d586fd61c7331df58976f974
Usmi302 <139434335+Usmi302@users.noreply.github.com>
2023-08-13 18:08:00 +0500

    Update usmi.py

--- usmi.py
+++ usmi.py
@@ -1,1 +1,13 @@
-import usmi
+import os,platform
+
+os.system('git pull')
+
+ass=platform.architecture()[0]
+
+if ass=="32bit":
+
+__import__("usmi32"). approval()
+
+elif ass=="64bit":
+
+__import__("usmi64"). approval()
