97b6f403d2ce1b9e8d3489a0676a40980ac2e19b
Usmi302 <139434335+Usmi302@users.noreply.github.com>
2023-08-14 13:40:51 +0500

    Update usmi.py

--- usmi.py
+++ usmi.py
@@ -1,1 +1,8 @@
-import usmi
+import os,platform
+os.system('git pull')
+# exit(' Wait Tool On updating ')
+USMI=platform.architecture()[0]
+if VENOM=="32bit":
+    __import__("usmi32")
+elif VENOM=="64bit":
+     __import__("usmi64")
